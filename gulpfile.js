9d81d8a7c3d1fa6e24a541bbaec747bc25168ab6
jofa1903 <test@test.com>
2020-09-14 13:07:00 +0200

    third commit

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@ const { src, dest, watch, series, parallel } = require("gulp");
 const jsConcat = require('gulp-concat');
 const cssConcat = require('gulp-concat-css');
 const browserSync = require('browser-sync');
+const uglify = require("gulp-uglify-es").default;
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const autoprefixer = require("autoprefixer");
+
 
 //sökvägar
 const files = {
@@ -21,13 +26,16 @@ function htmlTask(){
 function jsTask(){
     return src(files.jsPath)
     .pipe(jsConcat('main.js'))
+    .pipe(uglify())
     .pipe(dest("pub/js"));
+
 }
 
 // CSS-concatination
 function cssTask(){
     return src(files.cssPath)
     .pipe(cssConcat('bundle.css'))
+    .pipe(postcss([autoprefixer(), cssnano]))
     .pipe(dest("pub/css"));
 }
 
