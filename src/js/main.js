676d5ddaf60e762615048a1da9f414ec07efc697
jofa1903 <test@test.com>
2020-09-21 09:56:56 +0200

    second commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,3 @@
-// const navSlide = () => {
-//     const burger = document.querySelector('.burger');
-//     const nav = document.querySelector('.nav-links')
-//     const navLinks = document.querySelectorAll('.nav-links li')
-
-//     burger.addEventListener('click', () => {
-//         // nav toggle
-//         nav.classList.toggle('nav-active');
-//         // animate links
-//         navLinks.forEach((link, index) => {
-//             if(link.style.animation){
-//                 link.style.animation = '';
-//             } else{
-//                 links.style.animation = `navLinkFade 0.5s ease forwards ${index / 7  + 1.5}s`;
-//             }
-            
-//         });
-//     });
-
-// }
-
-// navSlide();
 
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
